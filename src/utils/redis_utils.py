1b0109ce2e82e25c5d47e6bf0a8eda1d6d6b1230
余海 <tree.yu@dadaabc.com>
2019-02-01 16:39:05 +0800

    调整代码

--- src/utils/redis_utils.py
+++ src/utils/redis_utils.py
@@ -8,17 +8,17 @@ _conn = None
 class RedisUtils(object):
 
     def __init__(self, host, port, db):
-        self.__conn = redis.ConnectionPool(host=host, port=port, db=db)
+        self._pool = redis.ConnectionPool(host=host, port=port, db=db)
 
-    def __get_redis(self):
-        return redis.Redis(connection_pool=self.__conn)
+    def _get_redis(self):
+        return redis.Redis(connection_pool=self._pool)
 
     def get(self, key):
-        r = self.__get_redis()
+        r = self._get_redis()
         return r.get(key)
 
     def set(self, key, val, time=0):
-        r = self.__get_redis()
+        r = self._get_redis()
         if time <= 0:
             r.set(key, val)
         else:
@@ -31,7 +31,7 @@ _redis_map = {}
 def get_redis_utils(host, port, db):
     global _redis_map
     key = '%s:%d:%d' % (host, port, db)
-    if None == _redis_map.get(key, None):
+    if None is _redis_map.get(key, None):
         _redis_map[key] = RedisUtils(host, port, db)
 
     return _redis_map[key]
