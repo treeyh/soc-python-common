193b3641ce0a5eca4ed4206ffbe5e719e158971d
Tree <tree@ejyi.com>
2019-07-24 15:55:33 +0800

    格式化生成sql

--- src/tools/sql/format_markdown_sql.py
+++ src/tools/sql/format_markdown_sql.py
@@ -85,8 +85,8 @@ file_sqls = [
         'comment' : '角色',
         'begin' : 1,
         'end' : -1,
-        'sql' : '''INSERT INTO ppm_tre_trends( `id`, `org_id`, `module1`, `module2`, `module3`, `oper_code`, `oper_obj_id`, `oper_obj_type`, `oper_obj_property`, `relation_id`, `relation_type`, `relation_obj_info`, `different_value`, `new_value`, `old_value`, `creator`, `create_time`, `is_delete` ) 
-        VALUES ( %s, %s, '%s', '%s', '%s', '%s', %s, '%s', '%s', %s, '%s', '%s', '', '', '', 1, now(), 0);''',
+        'sql' : '''INSERT INTO ppm_tre_trends( `id`, `org_id`, `module1`, `module2_id`, `module2`, `module3_id`, `module3`, `oper_code`, `oper_obj_id`, `oper_obj_type`, `oper_obj_property`, `relation_id`, `relation_type`, `relation_obj_info`, `new_value`, `old_value`, `creator`, `create_time`, `is_delete` ) 
+        VALUES ( %s, %s, '%s', %s, '%s', %s, '%s', '%s', %s, '%s', '%s', %s, '%s', '%s', '%s', '%s', 1, now(), 0);''',
     },
 
 ]
