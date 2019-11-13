60087e1b436abce9bb1560d42fc424b514612ba4
Tree <tree@ejyi.com>
2019-11-13 17:19:11 +0800

    调整代码

--- src/tools/jd_price.py
+++ src/tools/jd_price.py
@@ -9,15 +9,15 @@ import random
 syspath='D:\\00_BaiDuYun\\05_project\\00_selfapp\\04_tree_py_helper\\src'
 sys.path.append(syspath)
 
-from helper import str_helper, file_helper, http_helper
+from utils import http_utils, str_utils
 
 
 
 #http://p.3.cn/prices/mgets?skuIds=J_970602&type=1
 
 if __name__ == '__main__':
-    result = http_helper.http("http://p.3.cn/prices/mgets?skuIds=J_970602&type=1")
-    re = str_helper.json_decode(result)
+    result = http_utils.get("http://p.3.cn/prices/mgets?skuIds=J_970602&type=1")
+    re = str_utils.json_decode(result)
     print(re)
 
     print(re[0]['p'])
