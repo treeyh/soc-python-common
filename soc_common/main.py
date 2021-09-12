3d60f8416e55c93e2b2a7299ed2dec8e43491b35
Treeyh <tree@ejyi.com>
2021-09-12 09:25:03 +0800

    提交代码

--- soc_common/main.py
+++ soc_common/main.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 from soc_common.tools.code_generate.golang import generate_by_java
-from soc_common.tools.toolkit.config import convert_unit
+from soc_common.tools.toolkit.config import convert_unit, currency
 
 
 def main():
-  convert_unit.build_config()
+  # convert_unit.build_config()
+  currency.run()
 
 
 if __name__ == '__main__':
